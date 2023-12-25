f613b19770caf86ec9c9ee5cf5008ad02bb0f876
subh93 <das.subhashis0217@gmail.com>
2023-12-25 12:36:11 +0530

    Change in Suspense

--- src/components/namepics.jsx
+++ src/components/namepics.jsx
@@ -6,7 +6,7 @@ const Details = lazy(() => import('./details'));
 function namepics() {
   return (
     <div className='portrait:min-h-[81vh] landscape:min-h-[91vh] text-[#0A7029] flex justify-around items-center portrait:flex-col-reverse portrait:justify-around bg-gradient-to-b from-[#DBE8D8] from-30% to-[#C8DF52]'>
-    <Suspense fallback={<div className='bg-[#C8DF52]'>Loading....</div>}>
+    <Suspense fallback={<div className='bg-transparent font-[Kalam] font-bold text-[3vmax]'>Loading....</div>}>
       <Details/>
     </Suspense>
     </div>
